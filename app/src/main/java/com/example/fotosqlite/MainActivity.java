65dbab3bbdb24775b4576c6e07b7c8d682baef38
HalReal <halreal1508@gmail.com>
2023-08-28 16:22:06 -0600

    Primer cambio

---
+++ app/src/main/java/com/example/fotosqlite/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fotosqlite;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    MaterialButton Button;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button = findViewById(R.id.capture_picture_Id);
+        imageView = findViewById(R.id.ImageViewId);
+
+
+        ActivityResultLauncher<String> requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (isGranted) {
+                        Toast.makeText(this, "Permiso aceptado", Toast.LENGTH_SHORT).show();
+                        // Permission is granted. Continue the action or workflow in your
+                        // app.
+                    } else {
+                        // Explain to the user that the feature is unavailable because the
+                        // feature requires a permission that the user has denied. At the
+                        // same time, respect the user's decision. Don't link to system
+                        // settings in an effort to convince the user to change their
+                        // decision.
+                    }
+                });
+
+
+        Button.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this,"Acepto",Toast.LENGTH_SHORT).show();
+            } else {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA);
+            }
+        });
+    }
+}
